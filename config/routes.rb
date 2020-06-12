0e14d0bc3cfee9c3624870d75c7588e6e341bb4f
mamedona <66308805+mamedona@users.noreply.github.com>
2020-06-12 19:55:42 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get "/" => "schedules#index"
-  resources :answers
+  # resources :answers
   resources :schedules
+  root 'schedules#index' #トップページをschedulesコントローラのindexアクションに設定
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
